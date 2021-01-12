c07a024350b49585bec565017c3e0eca2a0b0af1
etmasi <hesalwayswithme61@gmail.com>
2021-01-12 23:22:52 +0200

    Week 2 lesson updated

---
+++ 02 Checkpoint/me.py
@@ -0,0 +1,7 @@
+yourName = input("What is your  name? ")
+
+yourMiddlename = input("What is your middle name? ")
+
+yourFamilyname= input("What is your suriname? ")
+
+print("my name is :" + " " + yourFamilyname + " " +  yourMiddlename + "  " +   yourName)
