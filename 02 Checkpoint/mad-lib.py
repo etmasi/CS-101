23d0f501be381a2eb17487145fa38486cd3999b8
etmasi <hesalwayswithme61@gmail.com>
2021-01-31 00:21:47 +0200

    updated lesson

---
+++ 02 Checkpoint/mad-lib.py
@@ -0,0 +1,14 @@
+myDescription = input(" Please enter the following:")
+myFeeling = input(" Adjective:")
+myPet = input(" Animal:")
+myWord = input(" verb:")
+myEctstasi = input("exclamation :")
+myWord = input(" verb:")
+myWord = input(" verb:")
+
+
+print(" My story: +The other day I was really in trouble. It all started when I saw a very "+  "myFeeling " +
++ "myPet" +  " " + "myword"+ " " + down the hallway.  + "my Estasi!"+ I yelled. But all
+I could think to do was to [verb] over and over. Miraculously,
+that caused it to stop, but not before it tried to + " " + myword + " " +
+right in front of my family.")
