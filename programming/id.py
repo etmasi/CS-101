cbce3c1d4ada1cfe3ccb6eb21b971b82fa08aa70
etmasi <hesalwayswithme61@gmail.com>
2021-02-12 19:32:41 +0200

    update assignment2

--- programming/id.py
+++ programming/id.py
@@ -10,3 +10,15 @@ id_number = input("ID Number: ")
 eye_color = input ("The color of your eye:")
 month = input ("month:")
 training = input("training completed:Yes/No")
+
+
+# \n  is being used to make a blank line before this:
+print("\nThe ID Card is:")
+print("----------------------------------------")
+print(f"{last.upper()}, {first.capitalize()}")
+print(job_title.title())
+print(f"ID: {id_number}")
+print()
+print(email.lower())
+print(phone)
+print("----------------------------------------")
