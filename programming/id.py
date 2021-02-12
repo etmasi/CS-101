184f989376fc3d1337b18e9140108d93e3b16f08
etmasi <hesalwayswithme61@gmail.com>
2021-02-12 19:21:33 +0200

    assignment2 updated

--- programming/id.py
+++ programming/id.py
@@ -1,7 +1,5 @@
 print("Please enter the following information:")
 
-# This prints a blank line
-print()
 
 first = input("First name: ")
 last = input("Last name: ")
@@ -9,14 +7,6 @@ email = input("Email address: ")
 phone = input("Phone number: ")
 job_title = input("Job title: ")
 id_number = input("ID Number: ")
-
-# This time I used a \n to make a blank line before this:
-print("\nThe ID Card is:")
-print("----------------------------------------")
-print({(last.upper), first.capitalize()}")
-print(job_title.title())
-print("ID: id_number")
-print()
-print(email.lower())
-print(phone)
-print("------------
+eye_color = input ("The color of your eye:")
+month = input ("month:")
+training = input("training completed:Yes/No")
