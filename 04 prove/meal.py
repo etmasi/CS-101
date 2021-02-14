2731dfaa8983e3c7e8ee324d3aec6cbb50a753e7
etmasi <hesalwayswithme61@gmail.com>
2021-02-14 03:03:19 +0200

    updated

--- 04 prove/meal.py
+++ 04 prove/meal.py
@@ -1,21 +1,25 @@
 child_meal =input (("Enter the price of the child's meal:"))
 adult_meal = input ( ("Enter the price of the adult's meal:"))
-meal_price = (float(child_meal) + float(adult_meal))
 
 
-childen_number = input("Enter the number of children:")
+children_number = input("Enter the number of children:")
 adults_number = input("Enter the number of adults:")
-people_number = (int(childen_number) + int(adults_number))
+print (int(children_number) + int(adults_number))
 sales_tax_rate = input("Enter sales tax:")
 
 
 
-subtotal = childen_number * child_meal + adults_number * adult_meal
+# subtotal = int(( children_number * child_meal) + int (adults_number * adult_meal))
+subtotal = (int(children_number) * int(child_meal)) + (int(adults_number) * int(adult_meal))
 
-sales_tax = print(subtotal * sales_tax_rate / 100)
-
-total_price = print(subtotal + sales_tax)
+sales_tax = print(subtotal * int(sales_tax_rate) / 100)
+total_price = print(subtotal + int(sales_tax_rate))
 
 payment = input( "How much did you pay?")
 
-change = input(payment - total_price))
+print(subtotal)
+print(sales_tax_rate)
+print(payment)
+print(total_price)
+
+change = int(payment) -  int(total_price)
